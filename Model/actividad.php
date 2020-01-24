c0587f85b2a78f8537c56dd627f0806acde266fa
anthonyca18m <anthonyca18m@gmail.com>
2020-01-24 10:21:28 -0500

    cg

---
+++ Model/actividad.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once("db_mysqli.php");
+
+
+if ($_POST["action"] == "aceptarActividad") {
+    $sql = "UPDATE actividad SET actividad.estado = 1 WHERE actividad.id = " . intval($_POST["codigo"]);
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        echo '
+        <script>
+
+                toastr["success"]("Actividad Aceptada", "Genial!");  
+            </script>
+        ';
+    } else {
+        echo '
+        <script>
+                toastr["warning"]("Ha ocurrido algo inesperado", "Ups!");  
+            </script>
+        ';
+    }
+}elseif ($_POST["action"] == "rechazarActividad") {
+
+    $sql = "UPDATE actividad SET actividad.estado = 2, actividad.mensaje = '".$rs["mns"]."' WHERE actividad.id = " . intval($_POST["codigo"]);
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        echo '
+        <script>
+                toastr["success"]("Ha sido rechazado con exitó la actividad", "Exitó!");  
+            </script>
+        ';
+    } else {
+        echo '
+        <script>
+                toastr["warning"]("Ha ocurrido algo inesperado", "Ups!");  
+            </script>
+        ';
+    }
+}
--- panelAdmin.php
+++ panelAdmin.php
@@ -214,52 +214,71 @@ if (!isset($_SESSION['usuario'])) {
                                         <div id="portfolioLoadMoreWrapper" class="row masonry" data-plugin-masonry="" data-plugin-options="{'itemSelector': '.masonry-item'}" data-total-pages="3" data-ajax-url="ajax/index-blog-4-ajax-load-more-">
                                             <?php
                                             include_once("Model/db_mysqli.php");
-                                            $sql = "SELECT * FROM actividad a INNER JOIN empresa e ON a.empresa = e.idEmpresa WHERE a.estado = 0";
+                                            $sql = "SELECT *, DAY(a.fecha_creacion) as diafc,
+                                                            MONTHNAME(a.fecha_creacion) as mesfc
+                                                     FROM actividad a INNER JOIN empresa e ON a.empresa = e.idEmpresa WHERE a.estado = 0";
                                             $result = mysqli_query($con, $sql);
 
                                             while ($rs = mysqli_fetch_array($result)) {
 
-                                                ?>
-                                                <div class="masonry-item no-default-style col-md-6">
-                                                    <article class="post post-medium border-0 pb-0 mb-5">
-                                                        <div class="post-image">
-                                                            <a href="blog-post.html">
-                                                                <img src="img\blog\medium\blog-46.jpg" 
-                                                                class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0 w-100" 
-                                                                alt="">
-                                                                <div class="date p-absolute z-index-2 top-10 right-10 mr-0 mr-3 pl-1 pt-1">
-                                                                    <span class="day bg-color-light font-weight-extra-bold py-2 text-color-dark">15</span>
-                                                                    <span class="month text-1 bg-color-light line-height-9 text-default w-100 top-2 d-block py-0">
-                                                                        <span class="text-1">JAN</span>
-                                                                    </span>
-                                                                </div>
-                                                            </a>
-                                                        </div>
-
-                                                        <div class="post-content bg-color-light p-4">
-
-                                                            <h2 class="font-weight-bold text-5 line-height-6 mt-0 mb-2">
-                                                                <?php echo $rs["titulo"]; ?>
-                                                            </h2>
-                                                            <p>
-                                                            <?php echo $rs["descripcion"]; ?>
-                                                            </p>
-
-                                                            <div class="post-meta m-0 p-0">
-                                                                <span><i class="far fa-user"></i> Publicado por <a href="#"><?php echo $rs["EnombreComercial"]; ?></a>
-                                                                </span>
-                                                                <span><i class="far fa-folder"></i> <a href="#">Photography</a>, <a href="#">Gadgets</a> </span>
-                                                                <span><i class="far fa-comments"></i> <a href="#">12
-                                                                        Comments</a></span>
-                                                                <span class="d-block mt-2"><a href="blog-post.html" class="btn btn-xs btn-light text-1 text-uppercase">Read
-                                                                        More</a></span>
+                                            ?>
+                                                <form id="actividadForm" method="POST">
+                                                    <input type="hidden" id="idActividad" name="idActividad" value="<?php echo $rs["id"] ?>">
+                                                    <div class="masonry-item no-default-style col-md-6">
+                                                        <article class="post post-medium border-0 pb-0 mb-5">
+                                                            <div class="post-image">
+                                                                <a>
+                                                                    <img src="img\blog\medium\blog-46.jpg" class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0 w-100" alt="">
+                                                                    <div class="date p-absolute z-index-2 top-10 right-10 mr-0 mr-3 pl-1 pt-1">
+                                                                        <span class="day bg-color-light font-weight-extra-bold py-2 text-color-dark">
+                                                                            <?php
+                                                                            echo $rs["diafc"];
+                                                                            ?>
+                                                                        </span>
+                                                                        <span class="month text-1 bg-color-light line-height-9 text-default w-100 top-2 d-block py-0">
+                                                                            <span class="text-1">
+                                                                                <?php
+                                                                                echo $rs["mesfc"];
+                                                                                ?>
+                                                                            </span>
+                                                                        </span>
+                                                                    </div>
+                                                                </a>
                                                             </div>
 
-                                                        </div>
-                                                    </article>
-                                                </div>
+                                                            <div class="post-content bg-color-light p-4">
+
+                                                                <h2 class="font-weight-bold text-5 line-height-6 mt-0 mb-2">
+                                                                    <?php echo $rs["titulo"]; ?>
+                                                                </h2>
+                                                                <p>
+                                                                    <?php echo $rs["descripcion"]; ?>
+                                                                </p>
+
+                                                                <div class="post-meta m-0 p-0">
+                                                                    <span><i class="far fa-user"></i> Publicado por <a href="#"><?php echo $rs["EnombreComercial"]; ?></a>
+                                                                    </span>
+                                                                    <span></span>
+                                                                    <span><i class="fas fa-users"></i> <a href="#">
+                                                                            <?php echo $rs["participantes_requeridos"] . " personas requeridas"; ?>
+                                                                        </a></span>
+                                                                    <span class="d-block mt-2">
+                                                                        <a id="btnAceptar" class="btn btn-xs btn-success text-1 text-uppercase text-light">
+                                                                            ACEPTAR
+                                                                        </a>
+                                                                        <a id="btnRechazar" data-toggle="modal" data-target="#rechazarActividad" class="btn btn-xs btn-danger text-1 text-uppercase text-light">
+                                                                            RECHAZAR
+                                                                        </a></span>
+
+                                                                </div>
+
+                                                            </div>
+                                                        </article>
+                                                    </div>
+                                                </form>
                                             <?php } ?>
                                         </div>
+                                        <div id="alerts"></div>
                                     </div>
 
                                 </div>
@@ -270,8 +289,73 @@ if (!isset($_SESSION['usuario'])) {
                     </div>
                 </div>
             </div>
-
+            <div class="modal fade" id="rechazarActividad" tabindex="-1" role="dialog" aria-labelledby="smallModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-sm">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title" id="smallModalLabel">Actividad</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        </div>
+                        <div class="modal-body">
+                            <form id="FormMnsActividad" method="POST">
+                            <input type="hidden" id="idActividadmns">
+                            <label>Ingresa motivo de rechazo de actividad: </label>
+                            <input type="text" id="mnsRechazo" name="mnsRechazo" class="form-control">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-success" id="btnRechazarModal" data-dismiss="modal">Enviar</button>
+                            <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
             <?php include_once("modals/modal_FormLogin.php"); ?>
 
 
             <?php include_once("layouts/footer.php"); ?>
+
+
+            <script>
+                $("#btnAceptar").on("click", function() {
+                    var codigo = $("#idActividad").val();
+                    var action = "aceptarActividad";
+                    $.ajax({
+                        url: 'Model/actividad.php',
+                        data: {
+                            codigo: codigo,
+                            action: action
+                        },
+                        type: 'post',
+                        beforeSend: function() {},
+                        success: function(data) {
+                            $("#alerts").html(data);
+                        }
+                    });
+                });
+                $("#btnRechazar").on("click", function() {
+                    var codigo = $("#idActividad").val();
+                    var action = "aceptarActividad";
+                    $("#idActividadmns").val(codigo);
+                });
+
+
+                $("#btnRechazarModal").on("click", function() {
+                    var codigo = $("#idActividadmns").val();
+                    var action = "rechazarActividad";
+                    var mns = $("#mnsRechazo").val();
+                    $.ajax({
+                        url: 'Model/actividad.php',
+                        data: {
+                            codigo: codigo,
+                            action: action,
+                            mns: mns
+                        },
+                        type: 'post',
+                        beforeSend: function() {},
+                        success: function(data) {
+                            $("#alerts").html(data);
+                        }
+                    });
+                });
+            </script>
